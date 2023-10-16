0efbc833797c9d2766e97a340b7904cb3b276440
Ahmed Abdien <ahmed.g.abdien@gmail.com>
2023-10-16 17:06:39 +0300

    Fetch data directly from api - no scoped variables: Program.cs: fix my mistakes-DbContext->DataContext ProductList(css): some styling

--- BlazorECommerce/Server/Program.cs
+++ BlazorECommerce/Server/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddDbContext<DbContext>(options =>
+builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 builder.Services.AddControllersWithViews();
