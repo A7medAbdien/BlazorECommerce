9095802df584ffae208542752f09be96368fc3eb
Ahmed Abdien <ahmed.g.abdien@gmail.com>
2023-10-25 18:48:20 +0300

    ALL: add product types admin and user role

--- BlazorECommerce/Server/Program.cs
+++ BlazorECommerce/Server/Program.cs
@@ -8,6 +8,7 @@ global using BlazorECommerce.Server.Services.AuthService;
 global using BlazorECommerce.Server.Services.OrderService;
 global using BlazorECommerce.Server.Services.PaymentService;
 global using BlazorECommerce.Server.Services.AddressService;
+global using BlazorECommerce.Server.Services.ProductTypeService;
 
 using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -32,6 +33,7 @@ builder.Services.AddScoped<ICartService, CartService>();
 builder.Services.AddScoped<IOrderService, OrderService>();
 builder.Services.AddScoped<IPaymentService, PaymentService>();
 builder.Services.AddScoped<IAddressService, AddressService>();
+builder.Services.AddScoped<IProductTypeService, ProductTypeService>();
 
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
